feebf6856a9ebdcdd3e3c39930df7ff0012a1f8e
Ananthu <ananthurpillai2002@gmail.com>
2025-03-03 02:29:04 +0530

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
+from blog.serializers import UserSerializers,PostSerializer
+from rest_framework import authentication,permissions
+from blog.models import Post
+
+
+class UserCreateView(CreateAPIView):
+
+    serializer_class=UserSerializers
+
+
+class PostListCreateView(ListAPIView,CreateAPIView):
+
+    authentication_classes=[authentication.BasicAuthentication]
+
+    permission_classes=[permissions.IsAuthenticated]
+
+    serializer_class=PostSerializer
+
+    queryset=Post.objects.all()
+
+    def perform_create(self,serializers):
+
+        return serializers.save(owner=self.request.user)
+
+
+class PostRetrieveUpdateDestroyView(RetrieveAPIView,UpdateAPIView,DestroyAPIView):
+
+    serializer_class=PostSerializer
+
+    queryset=Post.objects.all()
+
+    authentication_classes=[authentication.BasicAuthentication]
+
+    permission_classes=[permissions.IsAuthenticated]
+    
